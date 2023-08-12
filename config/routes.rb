4dee3bb8aade80c1a1671ae562ca0e29e76c6e13
Josiah <josiah.pilon@gmail.com>
2023-08-11 20:05:15 -0700

    feat: creates workspaces routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  root 'placeholders#show'
+  resources :workspaces
   
   resources :users, only: :create
-
+  
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/sessions', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  
+  root 'placeholders#show'
 end
